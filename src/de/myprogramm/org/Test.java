de349bdac5af11dc665511688d5a4ec7d6b8cb89
Devron <daniel.rhein84@googlemail.com>
2013-03-15 13:31:43 +0100

    Fehlende Daten hinzugefügt

---
+++ src/de/myprogramm/org/Test.java
@@ -0,0 +1,80 @@
+package de.myprogramm.org;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.sasl.RealmCallback;
+
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
+
+public class Test {
+
+static ModelControllerClient createClient(final InetAddress host, final int port,
+            final String username, final char[] password, final String securityRealmName) {
+
+final CallbackHandler callbackHandler = new CallbackHandler() {
+
+  public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+      for (Callback current : callbacks) {
+          if (current instanceof NameCallback) {
+              NameCallback ncb = (NameCallback) current;
+              ncb.setName(username);
+          } else if (current instanceof PasswordCallback) {
+              PasswordCallback pcb = (PasswordCallback) current;
+              pcb.setPassword(password.toString().toCharArray());
+          } else if (current instanceof RealmCallback) {
+              RealmCallback rcb = (RealmCallback) current;
+              rcb.setText(rcb.getDefaultText());
+          } else {
+              throw new UnsupportedCallbackException(current);
+          }
+      }
+  }
+};
+
+return ModelControllerClient.Factory.create(host, port, callbackHandler);
+}
+	
+	public static void main(String args[])
+	{
+		//Server auf Server-Adresse hierlocal host setzen
+		ModelControllerClient client = null;
+		ModelControllerClient unauthenticatedClient = null;
+		
+		try
+		{
+			InetAddress server = InetAddress.getByName("localhost");
+			//unauthenticatedClient = 
+			//		ModelControllerClient.Factory.create(server,9999);
+			unauthenticatedClient = Test.createClient(server, 9990, "sa", "sa".toCharArray(),"ManagementRealm");
+			 ModelNode testConnection = new ModelNode();
+			    testConnection.get("operation").set("read-resource");  // Execute an
+			    unauthenticatedClient.execute(testConnection);
+			    client = unauthenticatedClient;
+		}
+		catch(ConnectException ex)
+		{
+			System.err.println("Fehler bei dem Verbindungsaufbau:"+ ex.getLocalizedMessage());
+		}
+		catch(UnknownHostException ex)
+		{
+			System.err.println("Fehler bei Host:"+ ex.getLocalizedMessage());
+		}
+		//catch(Exception ex){
+			//ex.printStackTrace();
+		//}
+ catch (IOException e) {
+	 		System.err.println("Fehler bei dem Verbindungsaufbau:"+ e.getLocalizedMessage());
+		}
+		
+	}
+
+}
